23751ba0132790c35531b5f07046dce0062ac9e2
deepak <deepamdeebu@gmail.com>
2024-01-08 15:23:51 +0530

    Experience : Adding a basic layout for experience components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React, { useState } from 'react';
 import { Route, Switch } from "react-router-dom";
 import Header from './components/Header';
 import Home from './components/Home';
+import Experience from './components/Experience';
+
 
 function App() {
   return (
     <>
       <Header />
       <Switch>
+        <Route path="/experience">
+          <Experience />
+        </Route>
         <Route path="/">
           <Home />
         </Route>
