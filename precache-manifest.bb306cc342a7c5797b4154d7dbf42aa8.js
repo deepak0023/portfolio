bdd093276b8bd73838a8e1b8cacaae020b9639a6
deepak <deepamdeebu@gmail.com>
2024-01-08 18:48:08 +0530

    Updates

---
+++ precache-manifest.bb306cc342a7c5797b4154d7dbf42aa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d703eeb92561a61d30172b33d3b3ba87",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "76b00ca41c128edf439f",
+    "url": "/portfolio/static/css/main.e1213598.chunk.css"
+  },
+  {
+    "revision": "100f36b6f31f7730a020",
+    "url": "/portfolio/static/js/2.d60a81c3.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/portfolio/static/js/2.d60a81c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76b00ca41c128edf439f",
+    "url": "/portfolio/static/js/main.08435e55.chunk.js"
+  },
+  {
+    "revision": "7cfef013cf2ea0c04055",
+    "url": "/portfolio/static/js/runtime-main.3d9fd477.js"
+  }
+]);
