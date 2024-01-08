d9eb0f5cd11a919660282c199e059acee0cd3cbd
deepak <deepamdeebu@gmail.com>
2024-01-08 15:26:13 +0530

    Hobby Project : Adding basic layout for hobby project component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import Header from './components/Header';
 import Home from './components/Home';
 import Experience from './components/Experience';
+import HobbyProject from './components/HobbyProject';
 
 
 function App() {
@@ -12,6 +13,9 @@ function App() {
       <Switch>
         <Route path="/experience">
           <Experience />
+        </Route>
+        <Route path="/hobby_projects">
+          <HobbyProject />
         </Route>
         <Route path="/">
           <Home />
